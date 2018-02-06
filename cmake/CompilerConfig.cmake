6e3cdbe5a4a76fae46498b7d22d08808c19bf59e
Martin Shetty <martin.shetty@esss.se>
2018-02-06 14:49:16 +0100

    should run on Mac

--- cmake/CompilerConfig.cmake
+++ cmake/CompilerConfig.cmake
@@ -5,6 +5,10 @@ if (NOT CMAKE_CXX_STANDARD OR CMAKE_CXX_STANDARD LESS 14)
   set(CMAKE_CXX_STANDARD 14)
 endif ()
 
+if(APPLE)
+  add_definitions(-DUSE_BOOST_MUTEX=1)
+endif()
+
 if (CMAKE_CXX_COMPILER_ID MATCHES GNU)
   # Warnings
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -pedantic -Wextra")
