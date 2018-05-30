714cb4c2db594b60cc91dea46cdca6ce6684ac89
Martin Shetty <martukas@gmail.com>
2018-05-30 22:32:06 +0200

    some refactoring and improvements of acquire; updates #109'

--- source/cmd/main.cpp
+++ source/cmd/main.cpp
@@ -3,17 +3,9 @@
 #include "engine.h"
 
 #include "custom_logger.h"
-#include "lexical_extensions.h"
 
 #include "consumers_autoreg.h"
 #include "producers_autoreg.h"
-
-#include "producer_factory.h"
-#include "consumer_factory.h"
-
-#include "custom_timer.h"
-
-#include "h5json.h"
 
 #include "json_file.h"
 
@@ -22,108 +14,96 @@
 using namespace DAQuiri;
 
 std::atomic<bool> interruptor(false);
-void term_key(int /*sig*/)
-{
+void term_key(int /*sig*/) {
   interruptor.store(true);
 }
 
-int main(int argc, char** argv)
-{
-  signal(SIGINT, term_key);
+struct AcquireOptions {
+  CLI::App app;
+
+  uint64_t duration{0};
+  std::string profile_file;
+  std::string consumers;
+  bool verbose{false};
+  std::string save_h5;
+  std::string save_csv;
+
+  AcquireOptions() {
+    app = CLI::App{"acquire -- when having a GUI is just too much"};
+
+    app.add_option("-t,--time", duration, "How long shall we run?", true)
+        ->check(CLI::Range(uint64_t(1), std::numeric_limits<uint64_t>::max()));
+    app.add_option("-i,--input", profile_file, "DAQ producer config profile")
+        ->check(CLI::ExistingFile);
+    app.add_option("-o,--output", consumers, "DAQ consumer prototype project")
+        ->check(CLI::ExistingFile);
+    app.add_flag("-v,--verbose", verbose, "Print results");
+    app.add_option("-s,--save", save_h5, "Save to h5 file");
+    app.add_option("-c,--save_csv", save_csv, "Save to multiple csv files");
+  }
+};
+
+int main(int argc, char **argv) {
+  AcquireOptions opts;
+  CLI11_PARSE(opts.app, argc, argv);
 
   CustomLogger::initLogger(nullptr, "acquire");
   hdf5::error::Singleton::instance().auto_print(false);
   producers_autoreg();
   consumers_autoreg();
 
-  CLI::App app {"acquire -- when having a GUI is just too much"};
+  signal(SIGINT, term_key);
 
-  uint64_t duration {1};
-  app.add_option("-t,--time", duration, "How long shall we run?", true)
-      ->check(CLI::Range(uint64_t(1), std::numeric_limits<uint64_t>::max()));
+  auto &engine = Engine::singleton();
 
-  std::string profile_file;
-  app.add_option("-i,--input", profile_file, "DAQ producer config profile")
-      ->check(CLI::ExistingFile);
-
-  std::string consumers;
-  app.add_option("-o,--output", consumers, "DAQ consumer prototype project")
-      ->check(CLI::ExistingFile);
-
-  bool verbose {false};
-  app.add_flag("-v,--verbose", verbose, "Print results");
-
-  std::string save_h5;
-  app.add_option("-s,--save", save_h5, "Save to h5 file");
-
-  std::string save_csv;
-  app.add_option("-c,--save_csv", save_csv, "Save to multiple csv files");
-
-  CLI11_PARSE(app, argc, argv);
-
-  auto& engine = Engine::singleton();
-
-  nlohmann::json profile;
-  try
-  {
-    profile = from_json_file(profile_file);
-    engine.initialize(profile);
-  }
-  catch (std::exception& e)
-  {
-    ERR << "Failed to read engine config file '" << profile_file << "'. "
-        << "Json parsing error:\n"
-        << hdf5::error::print_nested(e, 1);
-    return EXIT_FAILURE;
+  if (!opts.profile_file.empty()) {
+    nlohmann::json profile;
+    try {
+      profile = from_json_file(opts.profile_file);
+      engine.initialize(profile);
+    }
+    catch (std::exception &e) {
+      ERR << "Failed to read engine config file '" << opts.profile_file << "'. "
+          << "Json parsing error:\n"
+          << hdf5::error::print_nested(e, 1);
+      return EXIT_FAILURE;
+    }
   }
 
-  if (verbose)
-    INFO << "\n" << engine.settings().debug("   ", false);
+  ProjectPtr project = ProjectPtr(new Project());
 
-  if (0 == (engine.status() & ProducerStatus::can_boot))
-  {
-    ERR << "Bad DAQ config. Engine cannot boot.";
-    return EXIT_FAILURE;
+  if (!opts.consumers.empty()) {
+    if (!hdf5::file::is_hdf5_file(opts.consumers)) {
+      ERR << "Supplied project prototype '" << opts.consumers << "' is not an hdf5 file. "
+          << "No consumer prototypes specified. Cannot acquire data.";
+      return EXIT_FAILURE;
+    }
+    project->open(opts.consumers);
   }
 
   engine.boot();
 
-  if (0 == (engine.status() & ProducerStatus::can_run))
-  {
-    ERR << "Bad DAQ config. Engine cannot run.";
-    return EXIT_FAILURE;
-  }
-
-  if (!hdf5::file::is_hdf5_file(consumers))
-  {
-    ERR << "Supplied project prototype '" << consumers << "' is not an hdf5 file. "
-        << "No consumer prototypes specified. Cannot acquire data.";
-    return EXIT_FAILURE;
-  }
-
-  ProjectPtr project = ProjectPtr(new Project());
-  project->open(consumers);
-
-  if (verbose)
+  if (opts.verbose) {
+    INFO << "Engine status:\n" << engine.settings().debug("   ", false);
     INFO << "Project before DAQ run:\n" << *project;
+  }
 
-  engine.acquire(project, interruptor, duration);
-  engine.die();
+  engine.acquire(project, interruptor, opts.duration);
 
-  if (verbose)
+  if (opts.verbose)
     INFO << "Project after DAQ run:\n" << *project;
 
-  if (!save_h5.empty())
-  {
-    INFO << "Saving h5 to " << save_h5;
-    project->save(save_h5);
+  if (!opts.save_h5.empty()) {
+    INFO << "Saving h5 to " << opts.save_h5;
+    project->save(opts.save_h5);
   }
 
-  if (!save_csv.empty())
-  {
-    INFO << "Saving csv to " << save_csv;
-    project->save_split(save_csv);
+  if (!opts.save_csv.empty()) {
+    INFO << "Saving csv to " << opts.save_csv;
+    project->save_split(opts.save_csv);
   }
+
+  engine.die();
 
   return EXIT_SUCCESS;
 }
