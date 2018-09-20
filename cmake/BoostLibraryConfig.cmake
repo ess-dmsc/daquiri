aa0ad9e93bdc6a7985f488d4c1268eca70bae6ee
Martin Shetty <martukas@gmail.com>
2018-09-20 02:13:41 -0400

    "also boost locale; updates #133"

--- cmake/BoostLibraryConfig.cmake
+++ cmake/BoostLibraryConfig.cmake
@@ -4,6 +4,6 @@ set(Boost_USE_STATIC_RUNTIME OFF)
 #set(Boost_USE_STATIC_RUNTIME ON)
 set(Boost_USE_SHARED_LIBS ON)
 
-find_package(Boost 1.41 COMPONENTS system algorithm lexical_cast REQUIRED)
+find_package(Boost 1.41 COMPONENTS system algorithm lexical_cast locale REQUIRED)
 
 add_definitions(-DBOOST_ALL_DYN_LINK)
