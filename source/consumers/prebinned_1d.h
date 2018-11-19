f899abe2135984b39ff3ac53045c3efffbc94716
Martin Shetty <martukas@gmail.com>
2018-11-19 08:27:51 +0100

    "downsampling for prebinned 1d; updates #158"

--- source/consumers/prebinned_1d.h
+++ source/consumers/prebinned_1d.h
@@ -2,37 +2,39 @@
 
 #include <consumers/spectrum.h>
 
-namespace DAQuiri {
+namespace DAQuiri
+{
 
 class Prebinned1D : public Spectrum
 {
-  public:
-    Prebinned1D();
-    Prebinned1D* clone() const override { return new Prebinned1D(*this); }
+ public:
+  Prebinned1D();
 
-  protected:
-    std::string my_type() const override { return "Prebinned 1D"; }
+  Prebinned1D* clone() const override
+  { return new Prebinned1D(*this); }
 
-    void _apply_attributes() override;
-    void _init_from_file() override;
-    void _recalc_axes() override;
+ protected:
+  std::string my_type() const override
+  { return "Prebinned 1D"; }
 
-    //event processing
-    void _push_event(const Event& event) override;
-    void _push_stats_pre(const Spill& spill) override;
-    bool _accept_spill(const Spill& spill) override;
-    bool _accept_events(const Spill& spill) override;
+  void _apply_attributes() override;
+  void _recalc_axes() override;
 
-    // cached parameters:
-    std::string trace_name_;
+  //event processing
+  void _push_event(const Event& event) override;
+  void _push_stats_pre(const Spill& spill) override;
+  bool _accept_spill(const Spill& spill) override;
+  bool _accept_events(const Spill& spill) override;
 
-    //from status manifest
-    int trace_idx_{-1};
+  // cached parameters:
+  std::string trace_name_;
+  uint16_t downsample_{0};
 
-    std::vector<double> domain_;
+  //from status manifest
+  int trace_idx_{-1};
 
-    //reserve memory
-    Entry entry_{{0}, 0};
+  //reserve memory
+  Entry entry_{{0}, 0};
 };
 
 }
