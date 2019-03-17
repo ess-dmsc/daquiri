297be5766262fb3a92201a38ec584ba907cb6f61
Morten Jagd Christensen <morten.christensen@esss.se>
2019-03-17 21:49:31 +0100

    manual conan commands for docker build

--- utils/first_build.sh
+++ utils/first_build.sh
@@ -5,7 +5,12 @@ pushd $(dirname "${BASH_SOURCE[0]}")/..
 rm -fr build
 mkdir -p build
 pushd build
-cmake ..
+
+conan install --build=boost_filesystem --options boost_filesystem:shared=True \
+      --options boost_system:shared=True boost_filesystem/1.69.0@bincrafters/stable || exit 1
+conan install --build=outdated .. || exit 1
+
+cmake -DCONAN=MANUAL ..
 make $@
 make all_tests $@
 source ./activate_run.sh
