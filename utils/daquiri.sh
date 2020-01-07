1d4efed8e383abd501920153659c8a01f9a478c6
Morten Jagd Christensen <morten.christensen@esss.se>
2020-01-07 13:11:40 +0100

    add check for environment variable before launching daquiri

--- utils/daquiri.sh
+++ utils/daquiri.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if ! [[ -z $DAQUIRI_KAFKA_BROKER ]]; then
+    echo "Environment setting DAQUIRI_KAFKA_BROKER is set to : $DAQUIRI_KAFKA_BROKER"
+    echo -n "proceed (y/n)? "
+    read YESNO
+    if [[ $YESNO != "y" ]]; then
+        echo "exiting ..."
+        exit
+    fi
+fi
+
 pushd $(dirname "${BASH_SOURCE[0]}")/../build
 
 source ./activate_run.sh
