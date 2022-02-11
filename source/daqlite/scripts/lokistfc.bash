4114c15917e7de8b4f06f969477ece494bbc9d94
MJC <morten.christensen@ess.eu>
2022-02-11 11:35:53 +0100

    added run script

---
+++ source/daqlite/scripts/lokistfc.bash
@@ -0,0 +1,3 @@
+../../../build/bin/daqlite -f ../configs/loki/stfc/loki.json &
+../../../build/bin/daqlite -f ../configs/loki/stfc/lokimon_ch0.json &
+../../../build/bin/daqlite -f ../configs/loki/stfc/lokimon_ch1.json
