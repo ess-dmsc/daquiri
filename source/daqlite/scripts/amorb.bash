8e3a8b71f24dff3dfd3e50bf69442dcec6c3e952
Morten Jagd Christensen <mortenjc@jcaps.com>
2022-05-05 11:01:18 +0200

    added amorb scripts

--- source/daqlite/scripts/amorb.bash
+++ source/daqlite/scripts/amorb.bash
@@ -1,3 +1,4 @@
 ../../../build/bin/daqlite -f ../configs/amorb/amor.json &
+../../../build/bin/daqlite -f ../configs/amorb/amormon.json &
 ../../../build/bin/daqlite -f ../configs/amorb/amortof.json &
 ../../../build/bin/daqlite -f ../configs/amorb/amortof2d.json
